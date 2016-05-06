be4f2d7461dc2ab3643cad4e20f123b29ca00912
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-05-06 16:58:38 +0100

    email replace in git history

--- scripts/git/change-email-git.sh
+++ scripts/git/change-email-git.sh
@@ -5,18 +5,18 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-git filter-branch --env-filter '
-OLD_EMAIL="$1"
-CORRECT_NAME="Lukasz Kaniowski"
-CORRECT_EMAIL="lukasz.kaniowski@gmail.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+git filter-branch --env-filter "
+OLD_EMAIL=\"$1\"
+CORRECT_NAME=\"Lukasz Kaniowski\"
+CORRECT_EMAIL=\"lukasz.kaniowski@gmail.com\"
+if [ \"\$GIT_COMMITTER_EMAIL\" = \"\$OLD_EMAIL\" ]
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    export GIT_COMMITTER_NAME=\"\$CORRECT_NAME\"
+    export GIT_COMMITTER_EMAIL=\"\$CORRECT_EMAIL\"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ \"\$GIT_AUTHOR_EMAIL\" = \"\$OLD_EMAIL\" ]
 then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    export GIT_AUTHOR_NAME=\"\$CORRECT_NAME\"
+    export GIT_AUTHOR_EMAIL=\"\$CORRECT_EMAIL\"
 fi
-' --tag-name-filter cat -- --branches --tags
+" --tag-name-filter cat -- --branches --tags
