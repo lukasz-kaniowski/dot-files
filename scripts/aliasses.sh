4e095fbeb2e7eab9286785693f635e2cd3e0ae44
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2018-11-23 13:19:44 +0000

    copy cloudwatch url and transform to awslogs cli cmd

--- scripts/aliasses.sh
+++ scripts/aliasses.sh
@@ -1,2 +1,9 @@
 export SCRIPTS_DIR="$HOME/Projects/moje/dot-files/scripts"
 alias cloudinary="$SCRIPTS_DIR/cloudinary/cloudinary"
+function Cw2awslogs() 
+{
+	pbpaste | sed -E 's/.*group=(.*);filter=(.*);start=(.*);end=(.*)/awslogs get \1 ALL --start="\3" --end="\4" --filter-pattern="\2"/' | pbcopy
+}
+alias cw2awslogs=Cw2awslogs
+
+
