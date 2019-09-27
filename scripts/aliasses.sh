517394c610e09dca30f840ddf54582c8c388520c
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2019-09-27 14:50:03 +0100

    Alias for shell notifications

--- scripts/aliasses.sh
+++ scripts/aliasses.sh
@@ -5,5 +5,6 @@ function Cw2awslogs()
 	pbpaste | sed -E 's/.*group=(.*);filter=(.*);start=(.*);end=(.*)/awslogs get \1 ALL --start="\3" --end="\4" --filter-pattern="\2"/' | pbcopy
 }
 alias cw2awslogs=Cw2awslogs
+alias notifyDone="osascript -e 'display notification \"This shit is done now\" with title \"Done\"'"
 
 
